a5f8ffbb21a9cb776ba22a1d0c694a623832b92b
1121415007 <1121415007@mail.hwu.edu.tw>
2025-10-22 02:04:15 +0800

    1022

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -1,27 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    public CharacterController charCtrl;
+    public CharacterController charChrl;
     public int HP;
 
-    // 初始化
+    //初始化
     void Start()
     {
         HP = 100;
     }
 
-    // 更新:偵測操作
+    //更新:偵測動作
     void Update()
     {
-      
+     
     }
 
-    public void Move()
+    public void Move(CallbackContext callback)
     {
         //角色控制器.移動(往前)
-        charCtrl.SimpleMove(Vector3.forward);
+        charChrl.SimpleMove(Vector3.forward);
     }
 }
